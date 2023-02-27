527881f24f83e93856ead62104629b041dcafa40
Luis Andres Villegas Sanchez <luisvillegassanchez6@gmail.com>
2023-02-27 19:16:35 +0000

    init commit

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import { Hono } from 'hono'
+import leaderboard from '../db/leaderboard.jsonx'
+const app = new Hono()
+
+app.get('/', (ctx) => {
+	return ctx.json([{
+		endpoint: "/leaderboard",
+		description: "Returns the leaderboard"
+	}])
+})
+
+app.get('/leaderboard', (ctx) => {
+	return ctx.json(leaderboard)
+})
+
+export default app
