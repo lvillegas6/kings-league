7630aedea4c1707f3c960edbeebba6ef01e6cb10
Luis Villegas <luisvillegassanchez6@gmail.com>
2023-03-07 12:29:45 -0500

    Table

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontSize: {
+        xxs: '.625rem'
+      },
+      fontFamily: {
+        body: ['Archivo', 'system-ui', 'sans-serif'],
+        title: ['Archivo Black', 'system-ui', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
 }
